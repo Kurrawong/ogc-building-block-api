757b7d3645e27972516a92701c2e94326234e5c1
hjohns <howiejohns@gmail.com>
2023-11-29 14:59:46 +1100

    3337ea6

---
+++ nuxt/about.b24af9c6.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./entry.28982f2f.js";const t={};function r(n,a){return o(),c("h1",null,"About Prez")}const _=e(t,[["render",r]]);export{_ as default};
---
+++ nuxt/sparql.34727d1c.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./entry.28982f2f.js";const o={};function t(n,s){return r(),c("h1",null,"SPARQL editor")}const _=e(o,[["render",t]]);export{_ as default};
